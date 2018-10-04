b8679a2940f61f9ada7b8e164a9a9183fc437943
regisjambois <33594622+regisjambois@users.noreply.github.com>
2018-10-04 16:57:40 +0200

    INIT

---
+++ src/serveur.ts
@@ -0,0 +1,40 @@
+
+/**
+ * SERVEUR SPARTA * 
+ */ 
+export class Serveur {
+
+    constructor() {
+        console.log('*');
+        
+    }
+
+    private displayServeurName() : void {
+        // http://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=Serveur%20SPARTA
+        console.log('  ███████╗███████╗██████╗ ██╗   ██╗███████╗██╗   ██╗██████╗  ');  
+        console.log('  ██╔════╝██╔════╝██╔══██╗██║   ██║██╔════╝██║   ██║██╔══██╗ ');  
+        console.log('  ███████╗█████╗  ██████╔╝██║   ██║█████╗  ██║   ██║██████╔╝ '); 
+        console.log('  ╚════██║██╔══╝  ██╔══██╗╚██╗ ██╔╝██╔══╝  ██║   ██║██╔══██╗ ');  
+        console.log('  ███████║███████╗██║  ██║ ╚████╔╝ ███████╗╚██████╔╝██║  ██║ ');  
+        console.log('  ╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝ ╚═════╝ ╚═╝  ╚═╝ ');  
+        console.log('');
+        console.log('      ███████╗██████╗  █████╗ ██████╗ ████████╗ █████╗ ');
+        console.log('      ██╔════╝██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗');
+        console.log('      ███████╗██████╔╝███████║██████╔╝   ██║   ███████║');
+        console.log('      ╚════██║██╔═══╝ ██╔══██║██╔══██╗   ██║   ██╔══██║');
+        console.log('      ███████║██║     ██║  ██║██║  ██║   ██║   ██║  ██║');
+        console.log('      ╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝');
+        console.log('');
+        console.log('');
+    }
+
+    public run() : void {
+        this.displayServeurName();
+    }
+}
+
+
+
+
+                                                                                                       
+                                                                                                       
---
+++ src/start.ts
@@ -0,0 +1,9 @@
+/**
+ * npm install -g ts-node
+ * ts-node start.ts
+ */
+import { Serveur }  from "./serveur";
+let srv = new Serveur();
+srv.run();
+
+
